6fed92f115717fef6d6c45c162a420d54aeca2d3
Abhishek Kumar Singh <abhishek81121@gmail.com>
2024-05-04 20:34:20 +0530

    added flutter secure storage to store access and referesh tokens

--- lib/data/repository/auth_init/auth_init_repository.dart
+++ lib/data/repository/auth_init/auth_init_repository.dart
@@ -1,11 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:manime/data/models/auth_init/auth_init.dart';
 
 class AuthInit {
   AuthInit();
   Future<Auth> authinit() async {
     final dio = Dio();
+    final storage = FlutterSecureStorage();
+
     Response response = await dio.post(
         'https://auth.mangadex.org/realms/mangadex/protocol/openid-connect/token',
         data: {
@@ -16,7 +19,10 @@ class AuthInit {
           'client_secret': dotenv.env['MANGADEX_API_SECRET']
         });
     if (response.statusCode == 200) {
-      return Auth.fromJson(response.data);
+      Auth auth = Auth.fromJson(response.data);
+      await storage.write(key: 'access_token', value: auth.access_token);
+      await storage.write(key: 'refresh_token', value: auth.refresh_token);
+      return auth;
     } else {
       return Auth(access_token: "", refresh_token: "");
     }
